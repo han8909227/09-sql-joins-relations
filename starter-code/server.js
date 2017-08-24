cd46f4d0e12f80f2d981de39a47d7433c4e060f9
han8909227 <hbao2016@hotmail.com>
2017-08-24 15:51:14 -0700

    done all to dos

--- starter-code/server.js
+++ starter-code/server.js
@@ -28,7 +28,7 @@ app.get('/articles', function(request, response) {
    SELECT  *
    FROM authors
    INNER JOIN articles
-   ON author_id = author_id;`)
+   ON authors.author_id = articles.author_id;`)
   .then(function(result) {
     response.send(result.rows);
   })
@@ -40,34 +40,49 @@ app.get('/articles', function(request, response) {
 app.post('/articles', function(request, response) {
   client.query(
     `INSERT INTO
-    author(author, "authorUrl")
-    VALUES ($1, $2);
+    authors(author, "authorUrl")
+    VALUES ($1, $2)
+    ON CONFLICT DO NOTHING;
     `,
     [
       request.body.author,
-      request.body.authorUrl,
+      request.body.authorUrl
     ], // TODO: Add the author and "authorUrl" as data for the SQL query
     function(err) {
       if (err) console.error(err)
-      queryTwo() // This is our second query, to be executed when this first query is complete.
+      queryTwo(); // This is our second query, to be executed when this first query is complete.
     }
   )
 
   function queryTwo() {
     client.query(
-      ``, // TODO: Write a SQL query to retrieve the author_id from the authors table for the new article
-      [], // TODO: Add the author name as data for the SQL query
+      `SELECT *
+      FROM authors
+      WHERE author = $1;
+      `, // TODO: Write a SQL query to retrieve the author_id from the authors table for the new article
+      [
+        request.body.author
+      ], // TODO: Add the author name as data for the SQL query
       function(err, result) {
         if (err) console.error(err)
-        queryThree(result.rows[0].author_id) // This is our third query, to be executed when the second is complete. We are also passing the author_id into our third query
+        queryThree(result.rows[0].author_id); // This is our third query, to be executed when the second is complete. We are also passing the author_id into our third query
       }
-    )
+    );
   }
 
   function queryThree(author_id) {
     client.query(
-      ``, // TODO: Write a SQL query to insert the new article using the author_id from our previous query
-      [], // TODO: Add the data from our new article, including the author_id, as data for the SQL query.
+      `INSERT INTO
+      articles(title, author_id, category, "publishedOn", body)
+      VALUES ($1, $2, $3, $4, $5);
+      `, // TODO: Write a SQL query to insert the new article using the author_id from our previous query
+      [
+        request.body.title,
+        author_id,
+        request.body.category,
+        request.body.publishedOn,
+        request.body.body,
+      ], // TODO: Add the data from our new article, including the author_id, as data for the SQL query.
       function(err) {
         if (err) console.error(err);
         response.send('insert complete');
@@ -81,16 +96,35 @@ app.put('/articles/:id', function(request, response) {
   // an author_id property, so we can reference it from the request.body.
   // TODO: Add the required values from the request as data for the SQL query to interpolate
   client.query(
-    ``,
-    []
+    `UPDATE authors
+    SET
+      author=$1, "authorUrl"=$2
+    WHERE author_id=$3;
+    `,
+    [
+      request.body.author,
+      request.body.authorUrl,
+      request.body.author_id
+    ]
   )
   .then(function() {
     // TODO: Write a SQL query to update an article record. Keep in mind that article records
     // now have an author_id, in addition to title, category, publishedOn, and body.
     // TODO: Add the required values from the request as data for the SQL query to interpolate
     client.query(
-      ``,
-      []
+      `UPDATE articles
+      SET
+        author_id = $1, title=$2, category=$3, "publishedOn"=$4, body=$5
+      WHERE article_id=$6;
+      `,
+      [
+        request.body.author_id,
+        request.body.title,
+        request.body.category,
+        request.body.publishedOn,
+        request.body.body,
+        request.params.id
+      ]
     )
   })
   .then(function() {
